351277b99aedef83595edcfec0b2c1b77173ea2a
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2021-04-05 17:18:49 -0500

    add ionic colors, default page, and modalcontroller
    - added ionic colors to the buttons
    - changed the default page to tab2
    - edited the modal component
    - added modal controller and dismissModal() function

--- src/app/components/tabs/tabs-routing.module.ts
+++ src/app/components/tabs/tabs-routing.module.ts
@@ -24,14 +24,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/tab2',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/tab2',
     pathMatch: 'full',
   },
 ];
--- src/app/modals/edit-recipe-modal/edit-recipe-modal.component.ts
+++ src/app/modals/edit-recipe-modal/edit-recipe-modal.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe, Ingredient } from '../../models/recipe';
 import { FirebaseService } from '../../services/firebase.service';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-edit-recipe-modal',
   templateUrl: 'edit-recipe-modal.component.html',
+  styleUrls: ['edit-recipe-modal.component.scss'],
 })
 export class EditRecipeModalComponent implements OnInit {
-  constructor(private firebase: FirebaseService) {}
+  constructor(
+    private firebase: FirebaseService,
+    public mdlctrl: ModalController
+  ) {}
   @Input() recipeName: string;
   @Input() source: string;
   @Input() pageNumber: number;
@@ -19,5 +24,9 @@ export class EditRecipeModalComponent implements OnInit {
 
   deleteRecipe(event, recipe: Recipe) {
     this.firebase.deleteRecipe(recipe);
+  }
+
+  dismissModal() {
+    this.mdlctrl.dismiss();
   }
 }
