cee326484dd89b76a41a17fb33163ca6a4da13a6
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2021-04-06 11:11:38 -0500

    renaming of tabs

--- src/app/components/tabs/tabs-routing.module.ts
+++ src/app/components/tabs/tabs-routing.module.ts
@@ -8,12 +8,12 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'new',
         loadChildren: () =>
           import('../tab1/tab1.module').then((m) => m.Tab1PageModule),
       },
       {
-        path: 'tab2',
+        path: 'recipes',
         loadChildren: () =>
           import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
       },
@@ -23,20 +23,20 @@ const routes: Routes = [
           import('../planner/planner.module').then((m) => m.PlannerPageModule),
       },
       {
-        path: 'tab3',
+        path: 'groceries',
         loadChildren: () =>
           import('../tab3/tab3.module').then((m) => m.Tab3PageModule),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab2',
+        redirectTo: '/tabs/planner',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab2',
+    redirectTo: '/tabs/planner',
     pathMatch: 'full',
   },
 ];
