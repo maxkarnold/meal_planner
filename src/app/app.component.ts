337f151067e48d879e61b61269798f983ad9a497
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2021-04-06 10:44:04 -0500

    updated firestore settings to ignore undefined fields
    also fixed the recipe.pageNumber and recipe.servings ngIf directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(db: AngularFirestore) {
+    const settings = { ignoreUndefinedProperties: true };
+    db.firestore.settings(settings);
+  }
 }
